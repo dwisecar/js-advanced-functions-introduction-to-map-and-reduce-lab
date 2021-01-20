4bd6f54b892031a95b1393ff5244260ee43bfbcb
dwisecar <dwisecar@gmail.com>
2021-01-19 19:54:39 -0800

    commit tree 02dd17ae05e851b98b8646ed3f7ce6030a29e7f3

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map(i => i * -1)
+}
+
+function mapToNoChange(arr){
+    return arr.map(i => i)
+}
+
+function mapToDouble(arr){
+    return arr.map(i => i * 2)
+}
+
+function mapToSquare(arr){
+    return arr.map(i => i * i)
+}
+
+function reduceToTotal(arr, start=0) {
+    let total = start
+    for (let i = 0; i < arr.length; i++ ) {
+      total = total + arr[i]
+    }
+    return total
+  }
+  
+  function reduceToAllTrue(arr) {
+    for (let i = 0; i < arr.length; i++ ) {
+      if (!arr[i]) return false
+    }
+    return true
+  }
+  
+  function reduceToAnyTrue(arr) {
+    for (let i = 0; i < arr.length; i++ ) {
+      if (arr[i]) return true
+    }
+    return false
+  }
